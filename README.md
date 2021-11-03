a770b4b631289deae14c5a5f2e7a284de66f18b1
RaviTeja6699 <vaggaraviteja63@gmail.com>
2021-11-03 14:03:57 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ctfdmg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ctfdmg)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let ravi=100;
+let  teja=200;
+var b=ravi+teja
+console.log(b);
