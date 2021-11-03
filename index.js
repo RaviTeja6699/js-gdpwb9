c65de431169014056a1a388da18576d6ed88abaa
RaviTeja6699 <vaggaraviteja63@gmail.com>
2021-11-03 15:10:21 +0530

    added operators

--- index.js
+++ index.js
@@ -8,4 +8,24 @@ let ravi=100;
 let  teja=200;
 var b=ravi+teja
 console.log(b);
+//operators
+var h=1000; 
+var j=20;
+var k='ravi'
+var i= '10'
+var l='teja'
+
+var m=h>j;
+var n=h<j;
+console.log(n);
+console.log(m);
+var o=h+k;
+console.log(o);
+//type caste it means automaically change the data type
+
+var p='150';
+var q='123';
+ var r=p-q;
+console.log(r);
+
 
